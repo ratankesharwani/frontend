93830f338c6dc5c0ed6a1f7f01d081f6d5546f08
Ratan Kesharwani <kesharwaniratan09@gmail.com>
2025-05-22 12:50:26 +0530

    Project list

--- src/app/projects/project-list/project-list.component.ts
+++ src/app/projects/project-list/project-list.component.ts
@@ -1,33 +1,60 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { MatIcon } from '@angular/material/icon';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { Location } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChip, MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-project-list',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatPaginatorModule, MatCardModule,MatIcon],
+  imports: [ CommonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatChipsModule,
+    MatChip,
+    RouterModule
+  ],
   templateUrl: './project-list.component.html',
   styleUrl: './project-list.component.scss'
 })
 export class ProjectListComponent {
-displayedColumns: string[] = ['title', 'description'];
-  dataSource = [
-    { title: 'Portfolio Site', description: 'Personal developer dashboard' },
-    { title: 'Analytics Dashboard', description: 'Admin charts and insights' },
-  ];
+  displayedColumns = ['title', 'description', 'tech_stack', 'milestones', 'user', 'actions'];
+  dataSource :any[] = []
+  private apiUrl = 'http://localhost:3000/projects';
 
-  constructor(private router: Router,private location: Location) {}
+  constructor(private router: Router, private location: Location, private fb: FormBuilder,
+    private http: HttpClient) { }
 
   goToNewProject() {
     this.router.navigate(['/dashboard/projects/new']);
   }
 
+  ngOnInit() {
+    this.http.get(this.apiUrl).subscribe({
+      next: (res: any) => (this.dataSource = res),
+      error: (err) => console.error('Fetching users failed', err)
+    });
+  }
+
   goBack() {
-  this.location.back();
-}
+    this.location.back();
+  }
+
+  updateProject($event:any){
+
+  }
+
+  removeProject($event:any){
+
+  }
 }
